878b1e8d446d1799deab21a8a61abb0c983962f0
Mariusz Sondecki <mariusz.sondecki@interia.pl>
2019-01-07 01:03:14 +0100

    Added structure for the project.

---
+++ src/main/java/com/sondecki/beansdepsspringbootactuator/BeansdepsSpringBootActuatorApplication.java
@@ -0,0 +1,14 @@
+package com.sondecki.beansdepsspringbootactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeansdepsSpringBootActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeansdepsSpringBootActuatorApplication.class, args);
+	}
+
+}
+
